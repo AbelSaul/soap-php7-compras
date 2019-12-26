41580e544a89e2db9a8c513478607dbd4f9a4948
Abel Saul Miraval Gomez <abelmiraval@hotmail.com>
2019-12-26 11:10:55 -0500

    Fixed changes

--- webservice-server.php
+++ webservice-server.php
@@ -5,13 +5,34 @@
  $server = new nusoap_server();
 
  /* Method to isnter a new compra */
-function guardarCompra($serie, $numero, $fecha_emision, $fecha_vencimiento,$operaciones_gravadas, $impueto_general_ventas, $importe_total, $moneda_id, $tipo_cambio_id,$tipo_comprobante_id, $tipo_pago_id, $proveedor_id){
+function guardarCompra($serie, $numero, $fecha_emision, $fecha_vencimiento,$operaciones_gravadas, $impuesto_general_ventas, $importe_total, $moneda_id, $tipo_cambio_id,$tipo_comprobante_id, $tipo_pago_id, $proveedor_id){
 
     global $dbconn;
-    $sql_insert = "insert into compras (serie, numero, fecha_emision, fecha_vencimiento, operaciones_gravadas) values ( :serie, :numero, :fecha_emision, :fecha_vencimiento, :operaciones_gravadas, :impuesto_general_ventas, :importe_total, :moneda_id, :tipo_cambio_id, :tipo_comprabante_id, :tipo_pago_id, proveedor_id)";
-    $stmt = $dbconn->prepare($sql_insert);
+    $sql_insert = "insert into compras
+     (
+      serie, numero, fecha_emision,
+      fecha_vencimiento, operaciones_gravadas,
+      impuesto_general_ventas,
+      importe_total, moneda_id, tipo_cambio_id,
+      tipo_comprobante_id, tipo_pago_id, proveedor_id
+     )
+     values
+     (
+      :serie, :numero, :fecha_emision,
+      :fecha_vencimiento, :operaciones_gravadas,
+      :impuesto_general_ventas,
+      :importe_total, :moneda_id, :tipo_cambio_id,
+      :tipo_comprobante_id, :tipo_pago_id, :proveedor_id
+      )";
+
+      $stmt = $dbconn->prepare($sql_insert);
     // insert a row
-    $result = $stmt->execute(array(':serie'=>$serie, ':numero'=>$numero, ':fecha_emision'=>$fecha_emision, ':fecha_vencimiento'=>$fecha_vencimiento, ':operaciones_gravadas'=>$operaciones_gravadas, ':impuesto_general_ventas'=> $impuesto_general_ventas, 'importe_total' => $importe_total, 'moneda_id' => $moneda_id, 'tipo_cambio_id'=> $tipo_cambio_id, 'tipo_comprobante_id' => $tipo_comprobante_id, 'tipo_pago_id' => $tipo_pago_id, 'proveedor_id'=> $proveedor_id));
+    $result = $stmt->execute(array(
+      ':serie'=>$serie, ':numero'=>$numero, ':fecha_emision'=>$fecha_emision,
+      ':fecha_vencimiento'=>$fecha_vencimiento,':operaciones_gravadas'=>$operaciones_gravadas,
+      ':impuesto_general_ventas'=> $impuesto_general_ventas,
+      ':importe_total' => $importe_total, ':moneda_id' => $moneda_id, ':tipo_cambio_id'=> $tipo_cambio_id, ':tipo_comprobante_id' => $tipo_comprobante_id, ':tipo_pago_id' => $tipo_pago_id, ':proveedor_id'=> $proveedor_id)
+    );
 
     if($result) {
       return json_encode(array('status'=> 200, 'msg'=> 'success'));
@@ -43,7 +64,7 @@ $server->register('obtenerDatosCompra',
 			'urn:compra#obtenerDatosCompra' //soapaction
       );
       $server->register('guardarCompra',
-			array('serie' => 'xsd:string', 'numero' => 'xsd:string', 'fecha_emision' => 'xsd:string', 'fecha_vencimiento' => 'xsd:string', 'operaciones_gravadas' => 'xsd:string' , 'impuesto_general_ventas' => 'xsd:string', 'importe_total' => 'xsd:string', 'moneda_id' => 'xsd:string', 'tipo_cambio_id' => 'xsd:string', 'tipo_cambio_id' => 'xsd:string', 'tipo_comprobante_id' => 'xsd:string', 'tipo_pago_id' => 'xsd:string', 'proveedor_id' => 'xsd:string'),  //parameter
+			array('serie' => 'xsd:string', 'numero' => 'xsd:string', 'fecha_emision' => 'xsd:string', 'fecha_vencimiento' => 'xsd:string', 'operaciones_gravadas' => 'xsd:string' , 'impuesto_general_ventas' => 'xsd:string', 'importe_total' => 'xsd:string', 'moneda_id' => 'xsd:string', 'tipo_cambio_id' => 'xsd:string','tipo_comprobante_id' => 'xsd:string', 'tipo_pago_id' => 'xsd:string', 'proveedor_id' => 'xsd:string'),  //parameter
 			array('data' => 'xsd:string'),  //output
 			'urn:compra',   //namespace
 			'urn:compra#obtenerDatosCompra' //soapaction
@@ -52,4 +73,7 @@ $server->service(file_get_contents("php://input"));
 
 // $datos = obtenerDatosCompra(1);
 // var_dump($datos);
+
+$datos = guardarCompra("B001","2","2019-12-24 08:05:36", "2019-12-24 08:05:36", "0","0","200", "1", "1", "1","1","1");
+var_dump($datos);
 ?>
